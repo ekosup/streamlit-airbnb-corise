4a22fb4adac8b7ce768338180e9ef44030bf8f3f
ekosup <67775889+ekosup@users.noreply.github.com>
2022-12-13 08:37:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import streamlit as st
 
 # Display title and text
-st.title("Week 1 - Data and visualization")
+st.title("Eko Supriyono - Data and visualization")
 st.markdown("Here we can see the dataframe created during this weeks project.")
 
 # Read dataframe
